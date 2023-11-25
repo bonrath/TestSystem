126981d9c1d422317734b3fa00c066093da76b7a
bonrathroeuth <bonrathroeuth@gmail.com>
2023-11-25 15:13:05 +0700

    first commit

---
+++ test1/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace test1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.name = "Show Your Home Page";
+            return View();
+        }
+        public ActionResult Showmore() 
+        {
+
+            return View("~/Views/About/Index.cshtml");
+        }
+        public int add() {
+            int a = 1;
+            int b = 4;
+            int r = a+ b;
+            return r;
+        
+        }
+    }
+}
